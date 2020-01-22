ebeff01ad6e1991ed08cceb3b96a73d1b3dcbeba
Stefano Rinaldi <s.rinaldi9@studenti.unipi.it>
2020-01-22 17:04:09 +0100

    tested

--- generate_color.py
+++ generate_color.py
@@ -12,12 +12,12 @@ using a Gaussian distribution for hosts and Schechter function for other galaxie
 Please notice that the hosts are required to be the first nev entries of the catalog.
 '''
 
-def Schechter(M, Ms, alpha, gal_den):
+def Schechter(M, Ms, alpha):
     '''
     Schechter function
     '''
     tmp = 10**(-0.4*(M-Ms))
-    return gal_den * tmp**(alpha+1.0)*np.exp(-tmp)
+    return tmp**(alpha+1.0)*np.exp(-tmp)
 
 if __name__ == '__main__':
 
@@ -25,19 +25,27 @@ if __name__ == '__main__':
     events = np.genfromtxt('2015_all_inj.txt', names = True)
     nev = events.shape[0]
 
+    # Schechter parameters as in O2 H0 paper (from Gehrels et al. (2016))
+    alpha   = -1.07
+    Mstar   = -20.47
+
+    # Gaussian Parameters
+    m_mean  = 20.
+    m_sigma = 0.5
+
     # Draw nev luminosities for hosts (gaussian distributed)
-    mag_hosts  = np.random.normal(lmean, lsigma, nev)
+    mag_hosts  = np.random.normal(m_mean, m_sigma, nev)
 
     mag_others = np.zeros(len(data)-nev)
     # Draw other galaxies luminosity (Schechter function) with Von Neumann's accept-reject sampling method
     mag_bounds = [-17, -24]
-    c = abs(mag_bounds[1]-mag_bounds[0])*Schechter(mag_bounds[0], Ms, alpha, gal_den)
+    c = abs(mag_bounds[1]-mag_bounds[0])*Schechter(mag_bounds[0], Mstar, alpha)
     for i in range(len(mag_others)):
         flag = True
         while(flag):
-            temp_mag = np.random.uniform(mag_bound[1],mag_bound[0])
+            temp_mag = np.random.uniform(mag_bounds[1],mag_bounds[0])
             att      = np.random.uniform(0, c)
-            bound    = Schechter(mag_bounds[0], Ms, alpha, gal_den)
+            bound    = Schechter(mag_bounds[0], Mstar, alpha)
             if(att < bound):
                 mag_others[i] = temp_mag
                 flag = False
