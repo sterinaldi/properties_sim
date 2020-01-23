e31ed4308131d78a95ebe30b3b2e713ad22f4cd0
Stefano Rinaldi <s.rinaldi9@studenti.unipi.it>
2020-01-23 09:31:49 +0100

    Implemented some plotting features

--- generate_color.py
+++ generate_color.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 from schechter_function import *
+import matplotlib.pyplot as plt
 
 '''
 Given a catalog and a subset of this catalog (the hosts - 'nev' objects),
@@ -12,43 +13,59 @@ using a Gaussian distribution for hosts and Schechter function for other galaxie
 
 Please notice that the hosts are required to be the first nev entries of the catalog.
 '''
+def gaussian(x,x0,sigma):
+    return np.exp(-(x-x0)**2/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
 
-if __name__ == '__main__':
+data   = pd.read_csv('MDC1_universe.txt', sep = ' ')
+events = np.genfromtxt('2015_all_inj.txt', names = True)
+nev = events.shape[0]
 
-    data   = pd.read_csv('MDC1_universe.txt', sep = ' ')
-    events = np.genfromtxt('2015_all_inj.txt', names = True)
-    nev = events.shape[0]
+# Schechter parameters as in O2 H0 paper (from Gehrels et al. (2016))
+alpha   = -1.07
+Mstar   = -20.47
 
-    # Schechter parameters as in O2 H0 paper (from Gehrels et al. (2016))
-    alpha   = -1.07
-    Mstar   = -20.47
+# Gaussian Parameters
+m_mean  = -20.
+m_sigma = 0.5
 
-    # Gaussian Parameters
-    m_mean  = -20.
-    m_sigma = 0.5
+# Draw nev luminosities for hosts (gaussian distributed)
+mag_hosts  = np.random.normal(m_mean, m_sigma, nev)
 
-    # Draw nev luminosities for hosts (gaussian distributed)
-    mag_hosts  = np.random.normal(m_mean, m_sigma, nev)
+mag_others = np.zeros(len(data)-nev)
+# Draw other galaxies luminosity (Schechter function) with Von Neumann's accept-reject sampling method
+mag_bounds = [-17, -24]
 
-    mag_others = np.zeros(len(data)-nev)
-    # Draw other galaxies luminosity (Schechter function) with Von Neumann's accept-reject sampling method
-    mag_bounds = [-17, -24]
+Schechter = SchechterMagFunction(mag_bounds[1], mag_bounds[0])
 
-    Schechter = SchechterMagFunction(mag_bounds[1], mag_bounds[0])
+c = Schechter(mag_bounds[0])
 
-    c = Schechter(mag_bounds[0])
+for i in range(len(mag_others)):
+    flag = True
+    while(flag):
+        temp_mag = np.random.uniform(mag_bounds[1],mag_bounds[0])
+        att      = np.random.uniform(0, c)
+        bound    = Schechter(temp_mag)
+        if(att < bound):
+            mag_others[i] = temp_mag
+            flag = False
 
-    for i in range(len(mag_others)):
-        flag = True
-        while(flag):
-            temp_mag = np.random.uniform(mag_bounds[1],mag_bounds[0])
-            att      = np.random.uniform(0, c)
-            bound    = Schechter(temp_mag)
-            if(att < bound):
-                mag_others[i] = temp_mag
-                flag = False
+magnitudes  = np.concatenate([mag_hosts, mag_others])
+data['mag'] = magnitudes
 
-    magnitudes  = np.concatenate([mag_hosts, mag_others])
-    data['mag'] = magnitudes
+data.to_csv('m-catalog.txt', header=True, index=False, sep='\t')
 
-    data.to_csv('m-catalog.txt', header=True, index=False, sep='\t')
+# Optional: check the output distribution
+output = True
+if(output):
+    plt.figure(1)
+    plt.subplot(211)
+    count, mbin, patch = plt.hist(magnitudes, density = True, bins = int(np.sqrt(len(magnitudes))))
+    app = np.linspace(min(mag_bounds),max(mag_bounds))
+    plt.plot(app, Schechter(app), label = 'Schechter')
+    plt.legend(loc=0)
+    meanbin = np.zeros(len(mbin)-1)
+    for i in range(len(mbin)-1):
+        meanbin[i] = (mbin[i+1]+mbin[i])/2.
+    plt.subplot(212)
+    plt.plot(meanbin, count-Schechter(meanbin))
+    plt.savefig('magnitude_histogram.pdf')
