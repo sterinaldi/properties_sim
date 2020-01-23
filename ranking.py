068cc42177bac3d73adfcb01234f63d83f97af19
Stefano Rinaldi <s.rinaldi9@studenti.unipi.it>
2020-01-23 10:25:15 +0100

    New catalog handling

--- ranking.py
+++ ranking.py
@@ -25,22 +25,6 @@ from scipy.stats import gaussian_kde
 
 from sklearn.mixture import GaussianMixture
 
-'''
-Given a GW observation with its posteriors on position and LD, this module
-ranks the galaxies localizated within the 95% region according to their probability
-of being the host of the compact binary system given a cosmology O.
-A = {alpha_i, delta_i, z_i}, X = {alpha_w, delta_w, LD, z_w}.
-
-p(A|X,O) = p(X|A,O)p(A,O)/p(X)
-
-p(X|A,O) = delta(alpha_i-alpha_w)delta(delta_i-delta_w)delta(z_i-z_w)delta(LD-f(z,O)) x
-           x p(alpha_w|O)p(delta_w|O)p(LD|O)p(z_i|O)
-
-given that we're making the assumption that the galaxy parameters are exactly known
-apart from redshift, where proper motion has to be taken into account.
-'''
-m_threshold = 19.0
-
 def Gaussexp(x, mu, sigma):
     return -(x-mu)**2/(2*sigma**2)-2.0*np.pi*sigma
 
@@ -66,52 +50,6 @@ def RedshiftCalculation(LD, omega, zinit=0.3, limit = 0.001):
     znew = zinit - (LD_test - LD)/dLumDist(zinit,omega)
     return RedshiftCalculation(LD, omega, zinit = znew)
 
-def GalInABox(ra, dec, ra_unit, dec_unit, catalog = 'GLADE2', all = False):
-    """
-    Given two RA, DEC intervals (thought as the boundaries of a shape), the function
-    returns a Pandas DataFrame with all the galaxies in CATALOG which position is within
-    the box.
-    Parameters
-    ----------
-    ra, dec: list
-        Intervals of angular coordinates within is fully included the desired region.
-
-    ra_unit, dec_unit: astropy.units.core.Unit
-        Units of RA and DEC
-        Default: deg
-
-    catalog: string, optional.
-        Catalog used for the analysis
-        Default: GLADE
-
-
-    all: boolean, optional
-        If all = True, returns all the columns downloaded from the catalog.
-        Otherwise, only RA, DEC e z.
-    Returns
-    -------
-    df: Pandas DataFrame
-        DataFrame Pandas containing all the selected objects.
-    """
-    if all:
-        v = Vizier()
-    else:
-        v = Vizier(columns = ['RAJ2000', 'DEJ2000', 'z', 'GWGC', 'BmagHyp', 'ImagHyp', 'Kmag2', 'Jmag2'])
-
-    v.ROW_LIMIT = 99999999
-    ra     = np.array(ra)
-    dec    = np.array(dec)
-    center = SkyCoord(ra.mean(), dec.mean(), unit = (ra_unit, dec_unit))
-    width  = abs(ra.max()-ra.min())/2.*ra_unit
-    height = abs(dec.max()-dec.min())/2.*dec_unit
-
-
-    table = v.query_region(center, radius = 1*u.deg, catalog = catalog) # width = width, height = height, catalog = catalog)
-    data  = pd.DataFrame()
-    # for tablei in table:
-    #     data = data.append(tablei.to_pandas(), ignore_index = True)
-    data = data.append(table[0].to_pandas())
-    return data.dropna()
 
 def get_samples(file, names = ['ra','dec','luminosity_distance']):
     filename, ext = splitext(file)
@@ -132,11 +70,11 @@ def get_samples(file, names = ['ra','dec','luminosity_distance']):
 
     if ext == '.hdf5':
         f = h5py.File(file, 'r')
-        dati = f[list(f.keys())[2]] # 2 low spin posteriors, 0 high spin posteriors
-        h5names = ['right_ascension','declination','luminosity_distance_Mpc']
+        data = f['lalinference_mcmc']['posterior_samples'][:]
+        h5names = ['ra','dec','dist']
 
         for name, nameh5 in zip(names, h5names):
-            samples[name] = dati[nameh5]
+            samples[name] = data[nameh5]
 
         return samples
 
@@ -178,14 +116,14 @@ def LD_posterior(LD_s):
 
 class ranking(cpnest.model.Model):
 
-    def __init__(self, omega, z_bounds):
+    def __init__(self, omega, z_bounds, catalog):
         self.names=['zgw']
         self.bounds=[z_bounds]
         self.omega   = omega
+        self.catalog = catalog
 
-    def GalInABox2(self, catalog):
+    def GalInABox(self):
         v = Vizier(columns = ['RAJ2000', 'DEJ2000', 'z', 'GWGC', 'Bmag', 'Jmag', 'Kmag', 'Hmag'])
-        v.ROW_LIMIT = 99999999
         center = SkyCoord(M.p_pos.means_[0][0], M.p_pos.means_[0][1], unit = (u.rad, u.rad))
         raggio = np.sqrt(np.diag(M.p_pos.covariances_[0])).max()
         table = v.query_region(center, radius = 5*raggio*u.rad, catalog = catalog) # width = width, height = height, catalog = catalog)
@@ -227,26 +165,6 @@ class ranking(cpnest.model.Model):
         plt.ylabel('$p(z)$')
         plt.savefig('pdfz.pdf')
 
-        self.catalog = self.catalog.sort_values('p', ascending = True)
-        plt.figure(4)
-        S = plt.scatter((self.catalog['Bmag']-self.catalog['Jmag']), (self.catalog['Jmag']-self.catalog['Kmag']), c = self.catalog['p'])
-        bar = plt.colorbar(S)
-        bar.set_label('p')
-        plt.xlabel('B-J')
-        plt.ylabel('J-K')
-        plt.savefig('colorplot.pdf', bbox_inches = 'tight')
-        self.catalog = self.catalog.sort_values('p', ascending = False)
-
-        plt.figure(5)
-        plt.plot(self.catalog['p'], self.catalog['Bmag'], marker = '.', ls ='', label = 'B')
-        plt.plot(self.catalog['p'], self.catalog['Jmag'], marker = '.', ls ='', label = 'J')
-        plt.plot(self.catalog['p'], self.catalog['Hmag'], marker = '.', ls ='', label = 'H')
-        plt.plot(self.catalog['p'], self.catalog['Kmag'], marker = '.', ls ='', label = 'K')
-        plt.legend(loc = 0)
-        plt.xlabel('p')
-        plt.ylabel('magnitude')
-        plt.savefig('p_colorindex.pdf', bbox_inches = 'tight')
-
     def get_names(self):
         names = []
         for ra, dec in zip(self.catalog['RAJ2000'], self.catalog['DEJ2000']):
@@ -270,16 +188,15 @@ class ranking(cpnest.model.Model):
         logL = np.log(Lh.sum())
         return logL
 
-    def run(self, file, show_output = False, run_sampling = True):
+    def run(self, file, id, show_output = False, run_sampling = True):
 
         # posteriors GW calculation
         samples = get_samples(file = file)
         self.pLD = gaussian_kde(samples['luminosity_distance'])
         self.p_pos = pos_posterior(samples['ra'],samples['dec'], number = 1)
         probs = []
-        self.catalog = self.GalInABox2(catalog='GLADE')
-        for ra, dec in zip(self.catalog['RAJ2000'], self.catalog['DEJ2000']):
-            probs.append(np.exp(self.p_pos.score_samples([[np.deg2rad(ra),np.deg2rad(dec)]]))[0]) # si riesce ad ottimizzare?
+        for ra, dec in zip(self.catalog['RA'], self.catalog['Dec']):
+            probs.append(np.exp(self.p_pos.score_samples([[ra,dec]]))[0]) # si riesce ad ottimizzare?
         self.catalog['ppos'] = np.array(probs)
         # Dropping galaxies outside the confident volume
         # Position
@@ -302,14 +219,14 @@ class ranking(cpnest.model.Model):
         self.catalog['p'] = prob
         self.catalog = self.catalog.sort_values('p', ascending = False)
         # self.get_names()
-        self.catalog.to_csv('rank.txt', header=True, index=None, sep='&', mode='w')
+        self.catalog.to_csv('rank'+str(id)+'.txt', header=True, index=None, sep='&', mode='w')
         if show_output:
             self.plot_outputs()
 
 if __name__ == '__main__':
 
     # Event selection
-    
+
     # positions = 'posterior_samples.json'
     # positions = 'GW170817_GWTC-1.hdf5'
     # positions = 'posterior_samples_170817.dat'
