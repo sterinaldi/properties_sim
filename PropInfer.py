5777ab66919b1d7ed15157bbbbbee85a553c245b
Stefano Rinaldi <s.rinaldi9@studenti.unipi.it>
2020-01-23 13:54:10 +0100

    Imported ranking.py

--- PropInfer.py
+++ PropInfer.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 import os
+import lal
 
 from ranking import *
 
@@ -23,6 +24,12 @@ catalog = catalog[catalog['z']<0.1]
 
 n_detections = len([f for f in os.listdir(path_to_data+'/lalinference_mcmc') if not f.startswith('.')])
 
+z_bounds = [0.0001,0.1]
+omega = lal.CreateCosmologicalParameters(0.7,0.3,0.7,-1.,0.,0.)
+
 for i in range(n_detections):
     id = str(i+1)
     posterior_file = path_to_data+'/lalinference_mcmc/'+id+'/posterior_samples.hdf5'
+    M = ranking(omega, z_bounds, catalog, id)
+    M.run(file = positions, run_sampling = True, show_output = True)
+    
